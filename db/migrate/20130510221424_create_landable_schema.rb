c41786c81cf2acde7a3a7c6242b2053ebba036dd
Kyle Hargraves <pd@krh.me>
2013-05-17 12:56:29 -0500

    UUIDs + pages.page_id PK name style
    Thoughtlessly breaking spec/internal for now, but that can be addressed
    a bit later once we have something really worth testing.

--- db/migrate/20130510221424_create_landable_schema.rb
+++ db/migrate/20130510221424_create_landable_schema.rb
@@ -1,27 +1,33 @@
 class CreateLandableSchema < ActiveRecord::Migration
   def change
-    create_table 'landable.paths' do |t|
+    enable_extension "uuid-ossp"
+
+    execute "CREATE SCHEMA landable"
+
+    create_table 'landable.layouts', id: :uuid, primary_key: :layout_id do |t|
+      t.string :name, null: false
+      t.text   :body, null: false
+      t.timestamps
+    end
+
+    create_table 'landable.paths', id: :uuid, primary_key: :path_id do |t|
       # bad name; this is probably '/foo' not 'http://...'
       t.text    :url,         null: false
       t.integer :status_code, null: false, default: 200
-      t.integer :page_id
+      t.uuid    :page_id
       t.timestamps
     end
 
     add_index 'landable.paths', :url, unique: true
 
-    create_table 'landable.pages' do |t|
-      t.text :title
-      t.text :body
+    create_table 'landable.pages', id: :uuid, primary_key: :page_id do |t|
+      t.uuid :layout_id
+      t.text :title, null: false
+      t.text :body,  null: false
       t.timestamps
     end
 
-    create_table 'landable.layouts' do |t|
-      t.string :name, null: false
-      t.timestamps
-    end
-
-    create_table 'landable.components' do |t|
+    create_table 'landable.components', id: :uuid, primary_key: :component_id do |t|
       t.string :name, null: false
 
       # TODO hstore? For now, just serialize a hash like a friggin caveman
@@ -30,8 +36,8 @@ class CreateLandableSchema < ActiveRecord::Migration
     end
 
     create_table 'landable.components_layouts', id: false do |t|
-      t.integer :component_id, null: false
-      t.integer :layout_id,    null: false
+      t.uuid :component_id, null: false
+      t.uuid :layout_id,    null: false
       t.text :config
       t.timestamps
     end
@@ -41,8 +47,8 @@ class CreateLandableSchema < ActiveRecord::Migration
       name: 'index_components_layouts'
 
     create_table 'landable.components_pages', id: false do |t|
-      t.integer :component_id, null: false
-      t.integer :page_id,      null: false
+      t.uuid :component_id, null: false
+      t.uuid :page_id,      null: false
       t.text :config
       t.timestamps
     end
--- spec/internal/db/schema.rb
+++ spec/internal/db/schema.rb
@@ -11,14 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130510221424) do
-
-  create_table "landable_pages", force: true do |t|
-    t.string   "title"
-    t.string   "state"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
