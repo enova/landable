6b670982f1b434d61de3612024b653772d445c10
Kyle Hargraves <pd@krh.me>
2013-05-22 14:33:52 -0500

    Expose :current_page helper in Public controller
    [#50307379]

--- app/controllers/landable/public/pages_controller.rb
+++ app/controllers/landable/public/pages_controller.rb
@@ -1,8 +1,10 @@
 module Landable
   module Public
     class PagesController < ApplicationController
+      helper_method :current_page
+
       def show
-        page = Page.by_path(request.path)
+        page = current_page
         case page.status_code
         when 200      then render_page(page)
         when 301, 302 then redirect_to page.redirect_url, status: page.status_code
@@ -15,6 +17,10 @@ module Landable
       def render_page(page)
         theme = page.theme
         render text: page.body, layout: theme.try(:layout) || 'application'
+      end
+
+      def current_page
+        @current_page ||= Page.by_path(request.path)
       end
     end
   end
