e201c2281a704cf0e17bf3f97ab39b83e2b6f422
Nikka Padilla <npadilla@enova.com>
2013-06-12 13:46:51 -0500

    theme name should be unique

--- app/models/landable/theme.rb
+++ app/models/landable/theme.rb
@@ -2,6 +2,7 @@ module Landable
   class Theme < ActiveRecord::Base
     self.table_name = 'landable.themes'
     validates_presence_of :name, :description
+    validates_uniqueness_of :name
     has_many :pages, inverse_of: :theme
   end
 end
