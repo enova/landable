0cd0a78691d46ff4f52cc9615b06858546483e2f
Isaac Bowen <ikebowen@gmail.com>
2013-06-13 10:34:33 -0500

    include a page's published_revision id in PageSerializer

--- app/serializers/landable/page_serializer.rb
+++ app/serializers/landable/page_serializer.rb
@@ -4,7 +4,9 @@ module Landable
     attributes :title, :body
     attributes :status_code, :redirect_url
     attributes :meta_tags
+
     has_one :theme, embed: :id
+    has_one :published_revision, embed: :id
 
     def meta_tags
       object.meta_tags || {}
