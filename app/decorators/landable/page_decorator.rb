06282ab5a995cd40d56779d9dadf563b0c6960d7
Kyle Hargraves <pd@krh.me>
2013-06-19 13:01:20 -0500

    New render pipeline obviates some of the decorator

--- app/decorators/landable/page_decorator.rb
+++ app/decorators/landable/page_decorator.rb
@@ -7,13 +7,8 @@ module Landable
       @page = page
     end
 
-    def head
-      (title + meta_tags).html_safe
-    end
-
     def title
-      title = page.title || default_title
-      content_tag('title', title)
+      content_tag('title', title) if page.title?
     end
 
     def path
@@ -33,25 +28,8 @@ module Landable
       }.compact.join("\n").html_safe
     end
 
-    def to_liquid
-      { 'head'      => head,
-        'title'     => title,
-        'meta_tags' => meta_tags,
-        'path'      => path,
-        'body'      => body
-      }
-    end
-
     private
 
     attr_reader :page # Keeping it private until there's a compelling reason not to
-
-    def default_title
-      if page.theme
-        "'#{page.theme.name}' theme; default title"
-      else
-        "unthemed; default title"
-      end
-    end
   end
 end
