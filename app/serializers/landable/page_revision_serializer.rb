490a9a90ddbc94201bb6e8fbd04dbcdc35d67087
Jesse Swartzendruber <jswartzendruber@enova.com>
2013-07-29 15:13:51 -0500

    Finish removing revisions theme_id.
    Missed some places the first time.

--- app/serializers/landable/page_revision_serializer.rb
+++ app/serializers/landable/page_revision_serializer.rb
@@ -10,21 +10,12 @@ module Landable
 
     embed :ids
     has_one :page
-    has_one :theme
     has_one :author, embed_key: :username, include: true, serializer: AuthorSerializer
 
     def snapshot_attributes
       attrs = object.snapshot_attributes[:attrs]
       attrs[:meta_tags] ||= {}
       attrs
-    end
-
-    def theme_id
-      snapshot_attributes[:theme_id]
-    end
-
-    def theme
-      Theme.find(theme_id) if theme_id
     end
   end
 end
