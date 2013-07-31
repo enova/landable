9edd4aff698628e64eb417ded783af2e0881348b
Jesse Swartzendruber <jswartzendruber@enova.com>
2013-07-31 16:09:06 -0500

    Fixed theme/template specs post-nullable thumbnail

--- spec/models/landable/template_spec.rb
+++ spec/models/landable/template_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 module Landable
   describe Template do
-    it { should_not have_valid(:thumbnail_url).when(nil) }
+    it { should have_valid(:thumbnail_url).when(nil) }
   end
 end
--- spec/models/landable/theme_spec.rb
+++ spec/models/landable/theme_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 module Landable
   describe Theme do
-    it { should_not have_valid(:thumbnail_url).when(nil) }
+    it { should have_valid(:thumbnail_url).when(nil) }
   end
 end
