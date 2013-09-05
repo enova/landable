23b48bc7fa71fb1fbde6418395b3b1488c0fe63a
Isaac Bowen <ikebowen@gmail.com>
2013-09-05 17:14:33 -0500

    use the correct foreign key for asset->author

--- app/serializers/landable/asset_serializer.rb
+++ app/serializers/landable/asset_serializer.rb
@@ -7,7 +7,7 @@ module Landable
 
     embed :ids
 
-    has_one  :author, embed_key: :username, include: true, serializer: AuthorSerializer
+    has_one  :author, include: true, serializer: AuthorSerializer
     has_many :pages
     has_many :themes
   end
