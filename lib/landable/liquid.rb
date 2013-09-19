1969c16ff3165968f28668340ff90b64b82eaf67
Isaac Bowen <ikebowen@gmail.com>
2013-09-19 16:33:55 -0500

    set up AssetTag for both app /and/ uploaded assets
    card #75

--- lib/landable/liquid.rb
+++ lib/landable/liquid.rb
@@ -1,126 +1,24 @@
 require 'liquid'
 
-module Landable
-  module Liquid
-    class Tag < ::Liquid::Tag
-      include ActionView::Helpers::TagHelper
+require 'landable/liquid/tags'
+require 'landable/liquid/asset_tags'
+require 'landable/liquid/filters'
 
-      protected
+# Tag generators
+Liquid::Template.register_tag('title_tag', Landable::Liquid::TitleTag)
+Liquid::Template.register_tag('meta_tags', Landable::Liquid::MetaTags)
+Liquid::Template.register_tag('head_tags', Landable::Liquid::HeadTags)
 
-      def lookup_page(context)
-        context.registers.fetch(:page) do
-          raise ArgumentError.new("`page' value was never registered with the template")
-        end
-      end
-    end
-
-    class AssetTag < Tag
-      attr_accessor :asset_name
-
-      protected
-
-      def lookup_asset(context, name)
-        assets = context.registers.fetch(:assets) do
-          raise ArgumentError.new("`assets' value was never registered with the template")
-        end
-
-        assets.fetch(name) do
-          raise ArgumentError.new("No `#{name}' asset available in #{assets.inspect}")
-        end
-      end
-    end
-
-    class Title < Tag
-      def render(context)
-        page = lookup_page context
-        content_tag(:title, page.title)
-      end
-    end
-
-    class MetaTags < Tag
-      def render(context)
-        page = lookup_page context
-        tags = page.meta_tags || {}
-
-        tags.map { |name, value|
-          tag(:meta, name: name, content: value)
-        }.join("\n")
-      end
-    end
-
-    class HeadTags < Tag
-      def render(context)
-        page = lookup_page context
-        head_tags = page.head_tags
-        head_tags.map(&:content).join("\n")
-      end
-    end
-
-    class Img < AssetTag
-      def initialize(name, param, tokens)
-        @asset_name = param.strip
-      end
-
-      def render(context)
-        asset = lookup_asset context, asset_name
-        tag :img, src: asset.public_url, alt: asset.description
-      end
-    end
-
-    class AssetAttribute < AssetTag
-      def initialize(tag, param, tokens)
-        @attribute  = tag.sub /^asset_/, ''
-        @asset_name = param.strip
-      end
-
-      def render(context)
-        asset = lookup_asset context, asset_name
-
-        case @attribute
-        when 'url' then asset.public_url
-        else asset.send @attribute
-        end
-      end
-    end
-
-    class Template < ::Liquid::Tag
-      def initialize(tag, param, tokens)
-        param_tokens = param.split(/\s+/)
-        @template_slug = param_tokens.shift
-        @variables = Hash[param_tokens.join(' ').scan(/([\w_]+):\s+"([^"]*)"/)]
-      end
-
-      def render(context)
-        template = Landable::Template.find_by_slug @template_slug
-
-        if template
-          ::Liquid::Template.parse(template.body).render @variables
-        else
-          "<!-- render error: missing template \"#{@template_slug}\" -->"
-        end
-      end
-    end
-
-    module DefaultFilter
-      def default(input, default_output=nil)
-        input.presence ? input : default_output
-      end
-    end
-  end
-
-  # Tag generators
-  ::Liquid::Template.register_tag('title_tag', Landable::Liquid::Title)
-  ::Liquid::Template.register_tag('meta_tags', Landable::Liquid::MetaTags)
-  ::Liquid::Template.register_tag('head_tags', Landable::Liquid::HeadTags)
-  ::Liquid::Template.register_tag('img_tag',   Landable::Liquid::Img)
-
-  # Only called tags so we can use a function-like syntax
-  ::Liquid::Template.register_tag('asset_url', Landable::Liquid::AssetAttribute)
-  ::Liquid::Template.register_tag('asset_description', Landable::Liquid::AssetAttribute)
-
-  # Template references
-  ::Liquid::Template.register_tag('template', Landable::Liquid::Template)
-
-  # Helpers
-  ::Liquid::Template.register_filter(Landable::Liquid::DefaultFilter)
+%w(img_tag image_tag javascript_include_tag stylesheet_link_tag).each do |tag|
+  Liquid::Template.register_tag(tag, Landable::Liquid::AssetTag)
 end
+
+# Only called tags so we can use a function-like syntax
+Liquid::Template.register_tag('asset_url', Landable::Liquid::AssetAttributeTag)
+Liquid::Template.register_tag('asset_description', Landable::Liquid::AssetAttributeTag)
+
+# Template references
+Liquid::Template.register_tag('template', Landable::Liquid::TemplateTag)
+
+# Helpers
+Liquid::Template.register_filter(Landable::Liquid::DefaultFilter)
