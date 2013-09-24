17e991fb9995798ce65ab4f762ed8b4be6dffab3
Jesse Swartzendruber <jswartzendruber@enova.com>
2013-09-24 10:23:28 -0500

    Fix page_revision name, now works too.

--- db/migrate/20130909182715_landable_page_revisions__break_out_snapshot.rb
+++ db/migrate/20130909182715_landable_page_revisions__break_out_snapshot.rb
@@ -1,27 +1,30 @@
+require_dependency 'landable/page_revision'
 class LandablePageRevisionsBreakOutSnapshot < ActiveRecord::Migration
+
+  class Landable::PageRevision < ActiveRecord::Base
+    store :snapshot_attributes, accessors: [:body]
+  end
+
   def change
 
     # Setup new columns
-    add_column "landable.page_revision", :theme_id,       :uuid
-    add_column "landable.page_revision", :status_code_id, :uuid
-    add_column "landable.page_revision", :category_id,    :uuid
-    add_column "landable.page_revision", :redirect_url,   :text
-    add_column "landable.page_revision", :body,           :text
-    add_column "landable.page_revision", :title,          :text
-    add_column "landable.page_revision", :path,           :text
-    add_column "landable.page_revision", :meta_tags,      :hstore
+    add_column "landable.page_revisions", :theme_id,       :uuid
+    add_column "landable.page_revisions", :status_code_id, :uuid
+    add_column "landable.page_revisions", :category_id,    :uuid
+    add_column "landable.page_revisions", :redirect_url,   :text
+    add_column "landable.page_revisions", :body,           :text
+    add_column "landable.page_revisions", :title,          :text
+    add_column "landable.page_revisions", :path,           :text
+    add_column "landable.page_revisions", :meta_tags,      :hstore
+    add_column "landable.page_revisions", :head_tags,      :hstore
 
     execute "ALTER TABLE landable.page_revisions ADD CONSTRAINT theme_id_fk FOREIGN KEY(theme_id) REFERENCES landable.themes(theme_id)"
     execute "ALTER TABLE landable.page_revisions ADD CONSTRAINT status_code_id_fk FOREIGN KEY(status_code_id) REFERENCES landable.status_codes(status_code_id)"
     execute "ALTER TABLE landable.page_revisions ADD CONSTRAINT category_id_fk FOREIGN KEY(category_id) REFERENCES landable.categories(category_id)"
 
-    class Landable::PageRevision
-      store :snapshot_attributes, accessors: [:body]
-    end
-
     # Go through each record and copy snapshot into new, broken-out columns
-    Landable::PageRevision.each do |rev|
-      page = Landable::Page.new rev.snapshot
+    Landable::PageRevision.all.each do |rev|
+      page = rev.snapshot
       rev.title = page.title
       rev.body = page.body
       rev.status_code_id = page.status_code_id
@@ -29,13 +32,21 @@ class LandablePageRevisionsBreakOutSnapshot < ActiveRecord::Migration
       rev.theme_id = page.theme_id
       rev.redirect_url = page.redirect_url
       rev.path = page.path
+      rev.head_tags = page.head_tags
       rev.save!
     end
 
     # Remove snapshot column
     remove_column "landable.page_revisions", :snapshot_attributes
 
-    # TODO: Create triggers preventing the sort of updates we just did :)
+    execute "DROP TRIGGER landable_page_revisions__no_update ON landable.page_revisions"
+
+    execute "CREATE TRIGGER landable_page_revisions__no_update
+            BEFORE UPDATE OF notes, is_minor, page_id, author_id, created_at, ordinal
+              , theme_id, status_code_id, category_id, redirect_url, body
+              , title, path, meta_tags, head_tags 
+            ON landable.page_revisions
+            FOR EACH STATEMENT EXECUTE PROCEDURE landable.tg_disallow();"
 
   end
 end
