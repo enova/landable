31fb653519afcd265bf53f160a42139f51943079
Isaac Bowen <ikebowen@gmail.com>
2013-09-26 10:04:36 -0500

    mount the landable engine during landable:install

--- lib/generators/landable/install_generator.rb
+++ lib/generators/landable/install_generator.rb
@@ -3,12 +3,16 @@ require 'rails/generators'
 module Landable
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path("../../templates", __FILE__)
+      source_root File.expand_path('../../templates', __FILE__)
 
-      desc 'Creates the Landable initializer'
+      desc 'Creates the Landable initializer and installs the engine route'
 
       def copy_initializer
         template 'landable.rb', 'config/initializers/landable.rb'
+      end
+
+      def add_landable_route
+        route 'mount Landable::Engine => \'/\' # move this to the end of your routes block'
       end
     end
   end
