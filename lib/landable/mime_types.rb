c7b53a3c4bccdfd37bc62af0b6c46fd89c45b3b7
Isaac Bowen <ikebowen@gmail.com>
2013-10-16 11:51:14 -0500

    unregister the :json type before re-registering

--- lib/landable/mime_types.rb
+++ lib/landable/mime_types.rb
@@ -3,6 +3,9 @@
 api_mime_types = %W(
   application/vnd.landable.v#{Landable::API_VERSION}+json
   application/vnd.landable+json
+  text/x-json
+  application/jsonrequest
 )
 
+Mime::Type.unregister :json
 Mime::Type.register 'application/json', :json, api_mime_types
