cfea99526d67082c478c06d95abc2daaa45f9ba2
Isaac Bowen <ikebowen@gmail.com>
2013-10-16 15:54:50 -0500

    consistent-ify the request payload for access tokens

--- app/controllers/landable/api/access_tokens_controller.rb
+++ app/controllers/landable/api/access_tokens_controller.rb
@@ -10,8 +10,9 @@ module Landable
       end
 
       def create
-        ident  = AuthenticationService.call(params[:username], params[:password])
+        ident  = AuthenticationService.call(asset_token_params[:username], asset_token_params[:password])
         author = RegistrationService.call(ident)
+
         respond_with AccessToken.create!(author: author), status: :created
       rescue Landable::AuthenticationFailedError
         head :unauthorized
@@ -20,6 +21,7 @@ module Landable
       def update
         token = find_own_access_token
         token.refresh!
+
         respond_with token
       end
 
@@ -27,15 +29,19 @@ module Landable
         token = find_own_access_token
         token.destroy!
         head :no_content
-
       rescue ActiveRecord::RecordNotFound
         head :unauthorized
       end
+
 
       private
 
       def find_own_access_token(id = params[:id])
         current_author.access_tokens.fresh.find(id)
+      end
+
+      def asset_token_params
+        params.require(:access_token).permit(:username, :password)
       end
     end
   end
