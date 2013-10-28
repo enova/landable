69b5505c452e0a1e700b2d8ca00cfc79f836484c
Isaac Bowen <ikebowen@gmail.com>
2013-10-28 10:28:53 -0500

    merge our two landable.action_controller initializers

--- lib/landable/engine.rb
+++ lib/landable/engine.rb
@@ -12,12 +12,6 @@ module Landable
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
     end
 
-    initializer "landable.action_controller" do
-      ActiveSupport.on_load :action_controller do
-        helper Landable::PagesHelper
-      end
-    end
-
     initializer "landable.enable_cors" do |app|
       config = Landable.configuration
       if config.cors.enabled?
@@ -50,8 +44,10 @@ module Landable
 
     initializer "landable.action_controller" do
       ActiveSupport.on_load :action_controller do
-        include Landable::Tracking
+        helper Landable::PagesHelper
 
+        # tracking
+        include Landable::Tracking
         prepend_around_action :track_with_landable!
       end
     end
