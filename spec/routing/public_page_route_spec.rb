c0e1f3c7eace4593d8598eec97c92ccd31132b00
Isaac Bowen <ikebowen@gmail.com>
2013-11-04 15:56:40 -0600

    tests!

---
+++ spec/routing/public_page_route_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe 'public page routes' do
+
+  routes { Landable::Engine.routes }
+
+  context 'should match' do
+    let(:author) { create :author }
+    let(:pages) do
+      [ create(:page, status_code: 200),
+        create(:page, status_code: 301, redirect_url: 'http://google.com/'),
+        create(:page, status_code: 302, redirect_url: 'http://foobar.com/'),
+      ].each do |page|
+        page.publish! author: author, status_code: 200
+      end
+    end
+
+    specify 'published pages' do
+      pages.each do |page|
+        expect(get: page.path).to route_to(controller: 'landable/public/pages', action: 'show', url: page.path[1..-1])
+      end
+    end
+  end
+
+  context 'should not match' do
+    specify 'random stuff' do
+      expect(get: '/foobar').to_not be_routable
+    end
+
+    specify 'unpublished pages' do
+      page = create :page
+      expect(get: page.path).to_not be_routable
+    end
+
+    specify '404 pages' do
+      page = create :page, status_code: 404
+      page.publish! author: create(:author)
+
+      expect(get: page.path).to_not be_routable
+    end
+  end
+
+end
