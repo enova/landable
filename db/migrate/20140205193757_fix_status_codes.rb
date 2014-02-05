0c49fcabbd29e6f45114c3b4a1f3df4316e7eb84
dkaplan <dkaplan@enova.com>
2014-02-05 14:27:10 -0600

    better memory usage in query

--- db/migrate/20140205193757_fix_status_codes.rb
+++ db/migrate/20140205193757_fix_status_codes.rb
@@ -3,14 +3,14 @@ class FixStatusCodes < ActiveRecord::Migration
     execute "DROP TRIGGER landable_page_revisions__no_update ON landable.page_revisions"
 
     # Find Pages with 404s, switch to 410s, publish the page!
-    Landable::Page.where(status_code: 404).each do |page| 
+    Landable::Page.where(status_code: 404).find_each do |page| 
       page.status_code = 410
       page.save!
       page.publish!
     end
 
     # Find Remaining PageRevisions with 404s, switch to 410s!
-    Landable::PageRevision.where(status_code: 404).each do |page| 
+    Landable::PageRevision.where(status_code: 404).find_each do |page| 
       page.status_code = 410
       page.save!
     end
