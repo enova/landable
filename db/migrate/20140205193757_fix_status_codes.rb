b8596314010f200057bf9b93b5a46e10bb8dafa5
dkaplan <dkaplan@enova.com>
2014-02-05 14:22:47 -0600

    Migration Handles Publishing 410 Pages, and Remaining PageRevisions

--- db/migrate/20140205193757_fix_status_codes.rb
+++ db/migrate/20140205193757_fix_status_codes.rb
@@ -1,13 +1,24 @@
 class FixStatusCodes < ActiveRecord::Migration
   def up
+    execute "DROP TRIGGER landable_page_revisions__no_update ON landable.page_revisions"
+
+    # Find Pages with 404s, switch to 410s, publish the page!
     Landable::Page.where(status_code: 404).each do |page| 
       page.status_code = 410
       page.save!
+      page.publish!
     end
 
+    # Find Remaining PageRevisions with 404s, switch to 410s!
     Landable::PageRevision.where(status_code: 404).each do |page| 
       page.status_code = 410
       page.save!
     end
+
+    execute "CREATE TRIGGER landable_page_revisions__no_update
+            BEFORE UPDATE OF notes, is_minor, page_id, author_id, created_at, ordinal
+              , theme_id, status_code, category_id, redirect_url, body
+            ON landable.page_revisions
+            FOR EACH STATEMENT EXECUTE PROCEDURE landable.tg_disallow();"
   end
 end
