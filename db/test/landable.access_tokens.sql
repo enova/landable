dae76cc9ec13250357c5fc083c62d4fb9c72d18f
Jesse Swartzendruber <jswartzendruber@enova.com>
2014-02-17 13:13:35 -0600

    Fix DB unit tests

--- db/test/landable.access_tokens.sql
+++ db/test/landable.access_tokens.sql
@@ -1,14 +1,12 @@
 BEGIN;
 
-  SELECT PLAN(5);
+  SELECT PLAN(4);
 
-  SELECT col_is_pk('landable', 'access_tokens', 'access_token_id', 'access tokens have PK');
-  SELECT col_is_fk('landable', 'access_tokens', 'author_id', 'author_id is FK');
+  SELECT col_is_pk('dummy_landable', 'access_tokens', 'access_token_id', 'access tokens have PK');
+  SELECT col_is_fk('dummy_landable', 'access_tokens', 'author_id', 'author_id is FK');
 
-  SELECT col_not_null('landable', 'access_tokens', 'author_id', 'author_id not null');
-  SELECT col_not_null('landable', 'access_tokens', 'expires_at', 'expires_at not null');
-
-  SELECT indexes_are('landable', 'access_tokens', ARRAY['landable_access_tokens__author_id', 'access_tokens_pkey']);
+  SELECT col_not_null('dummy_landable', 'access_tokens', 'author_id', 'author_id not null');
+  SELECT col_not_null('dummy_landable', 'access_tokens', 'expires_at', 'expires_at not null');
 
   SELECT * FROM finish();
 
--- db/test/landable.themes.sql
+++ db/test/landable.themes.sql
@@ -2,23 +2,23 @@ BEGIN;
 
   SELECT PLAN(12);
 
-  SELECT col_is_pk('landable', 'themes', 'theme_id', 'Theme_id is PK');
+  SELECT col_is_pk('dummy_landable', 'themes', 'theme_id', 'Theme_id is PK');
 
-  SELECT col_not_null('landable', 'themes', 'name', 'Name is not null');
-  SELECT col_not_null('landable', 'themes', 'body', 'body is not null');
-  SELECT col_not_null('landable', 'themes', 'description', 'Description is not null');
-  SELECT col_not_null('landable', 'themes', 'editable', 'Editable is not null');
+  SELECT col_not_null('dummy_landable', 'themes', 'name', 'Name is not null');
+  SELECT col_not_null('dummy_landable', 'themes', 'body', 'body is not null');
+  SELECT col_not_null('dummy_landable', 'themes', 'description', 'Description is not null');
+  SELECT col_not_null('dummy_landable', 'themes', 'editable', 'Editable is not null');
 
-  SELECT col_has_default('landable', 'themes', 'editable', $$Editable has default.$$);
+  SELECT col_has_default('dummy_landable', 'themes', 'editable', $$Editable has default.$$);
 
   --Verify unique index on theme name
-  SELECT lives_ok($$INSERT INTO landable.themes (name, body, description, editable) VALUES ('test', 'body', 'test body', true)$$);
-  SELECT throws_matching($$INSERT INTO landable.themes (name, body, description) VALUES ('test', 'body', 'test body')$$, 'name');
-  SELECT throws_matching($$INSERT INTO landable.themes (name, body, description) VALUES ('TEST', 'body', 'test body')$$, 'name');
+  SELECT lives_ok($$INSERT INTO dummy_landable.themes (name, body, description, editable) VALUES ('test', 'body', 'test body', true)$$);
+  SELECT throws_matching($$INSERT INTO dummy_landable.themes (name, body, description) VALUES ('test', 'body', 'test body')$$, 'name');
+  SELECT throws_matching($$INSERT INTO dummy_landable.themes (name, body, description) VALUES ('TEST', 'body', 'test body')$$, 'name');
 
-  SELECT index_is_unique('landable', 'themes', 'landable_themes__u_file', $$Unique index on file column.$$);
-  SELECT lives_ok($$INSERT INTO landable.themes (file, name, body, description, editable) VALUES ('filename', 'test1', 'body', 'test body', true)$$);
-  SELECT throws_matching($$INSERT INTO landable.themes (file, name, body, description) VALUES ('FILENAME', 'test1', 'body', 'test body')$$, '__u_file');
+  SELECT index_is_unique('dummy_landable', 'themes', 'dummy_landable_themes__u_file', $$Unique index on file column.$$);
+  SELECT lives_ok($$INSERT INTO dummy_landable.themes (file, name, body, description, editable) VALUES ('filename', 'test1', 'body', 'test body', true)$$);
+  SELECT throws_matching($$INSERT INTO dummy_landable.themes (file, name, body, description) VALUES ('FILENAME', 'test1', 'body', 'test body')$$, '__u_file');
 
   SELECT * FROM finish();
 
