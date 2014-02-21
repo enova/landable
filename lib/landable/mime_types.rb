1a9088b2ce185937c85dab095d1f27b5ea4aa158
Isaac Bowen <ikebowen@gmail.com>
2014-02-21 11:18:10 -0600

    add a todo - at some point we should be able to be arbitrarily precise when requesting an api version

--- lib/landable/mime_types.rb
+++ lib/landable/mime_types.rb
@@ -1,5 +1,8 @@
 # Re-register application/json, adding our own vendored mime types as aliases
 
+# TODO Be more generous about accepting version constraints. Currently,
+# requesting v1.5 when we're running v1.5.2 will return a 406.
+
 api_mime_types = %W(
   application/vnd.landable.v#{Landable::VERSION::MAJOR}+json
   application/vnd.landable.v#{Landable::VERSION::STRING}+json
