028049ba8782d4c0a03ff1191bcf951b801c6953
Jesse Swartzendruber <jswartzendruber@enova.com>
2014-02-25 09:34:06 -0600

    Update migrations to new schema format

--- db/migrate/20140220174630_add_abstract_and_hero_asset_to_pages_and_page_revisions.rb
+++ db/migrate/20140220174630_add_abstract_and_hero_asset_to_pages_and_page_revisions.rb
@@ -1,8 +1,8 @@
 class AddAbstractAndHeroAssetToPagesAndPageRevisions < Landable::Migration
   def change
-    add_column 'landable.pages', :abstract, :text
-    add_column 'landable.pages', :hero_asset_id, :uuid
-    add_column 'landable.page_revisions', :abstract, :text
-    add_column 'landable.page_revisions', :hero_asset_id, :uuid
+    add_column "#{Landable.configuration.database_schema_prefix}landable.pages", :abstract, :text
+    add_column "#{Landable.configuration.database_schema_prefix}landable.pages", :hero_asset_id, :uuid
+    add_column "#{Landable.configuration.database_schema_prefix}landable.page_revisions", :abstract, :text
+    add_column "#{Landable.configuration.database_schema_prefix}landable.page_revisions", :hero_asset_id, :uuid
   end
 end
