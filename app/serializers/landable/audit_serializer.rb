66e1ac37c92d6437597a7fdbd5304212988260ba
dkaplan <dkaplan@enova.com>
2014-05-29 15:08:08 -0500

    send created_at along with json/serializer for audits

--- app/serializers/landable/audit_serializer.rb
+++ app/serializers/landable/audit_serializer.rb
@@ -1,7 +1,7 @@
 module Landable
   class AuditSerializer < ActiveModel::Serializer
     attributes :id, :flags, :notes, :approver
-    attributes :auditable_type, :auditable_id
+    attributes :auditable_type, :auditable_id, :created_at
 
     def auditable_type
       object.auditable_type == 'Landable::Page' ? 'page' : 'template'
