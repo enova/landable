12bca4967aa4ff53099bf424d4ab313efd5ea20e
Grant Evans <gevans@enova.com>
2014-06-02 17:11:00 -0500

    path_response_time_view: fixed extra _ in landable configuration calls.

--- db/migrate/20140602213937_path_response_time_view.rb
+++ db/migrate/20140602213937_path_response_time_view.rb
@@ -2,7 +2,7 @@ class PathResponseTimeView < ActiveRecord::Migration
   def up
   	execute <<-SQL
   	DROP VIEW IF EXISTS "#{Landable.configuration.database_schema_prefix}landable_traffic.path_response_time__by_time";
-	CREATE OR REPLACE VIEW "#{Landable.configuration.database_schema_prefix}_landable_traffic.path_response_time__by_time"
+	CREATE OR REPLACE VIEW "#{Landable.configuration.database_schema_prefix}landable_traffic.path_response_time__by_time"
 	AS
 	SELECT * 
 	FROM 
@@ -19,7 +19,7 @@ class PathResponseTimeView < ActiveRecord::Migration
 	order by agg1."average response time (ms)" desc
 	;
 
-	COMMENT ON VIEW "#{Landable.configuration.database_schema_prefix}_landable_traffic.path_response_time__by_time" IS $$ The view path_response_time__by_time returns the paths displayed and the average reponse time (in ms) for each page of the site.  This list is ordered by response time, decending.$$;
+	COMMENT ON VIEW "#{Landable.configuration.database_schema_prefix}landable_traffic.path_response_time__by_time" IS $$ The view path_response_time__by_time returns the paths displayed and the average reponse time (in ms) for each page of the site.  This list is ordered by response time, decending.$$;
   SQL
   end
 end
