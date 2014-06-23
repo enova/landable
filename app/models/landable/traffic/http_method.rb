873723f15d942ce424640856cd7c37af4998d891
dkaplan <dkaplan@enova.com>
2014-06-23 10:37:28 -0500

    updating lookup_by :http_method

--- app/models/landable/traffic/http_method.rb
+++ app/models/landable/traffic/http_method.rb
@@ -3,7 +3,7 @@ module Landable
     class HTTPMethod < ActiveRecord::Base
       include Landable::Traffic::TableName
 
-      lookup_by :http_method, cache: true, find_or_create: true
+      lookup_by :http_method, cache: 50, find_or_create: true
 
       has_many :page_views
     end
