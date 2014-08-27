8d2710c2179e864be7f6011542fa9933190ca094
dkaplan <dkaplan@enova.com>
2014-08-27 13:50:04 -0500

    Extending Category API to include Create / Update

--- app/controllers/landable/api/categories_controller.rb
+++ app/controllers/landable/api/categories_controller.rb
@@ -5,9 +5,28 @@ module Landable
         respond_with Category.all
       end
 
+      def create
+        category = Category.new(category_params)
+        category.save!
+
+        respond_with category, status: :created, location: category_url(category)
+      end
+
+      def update
+        category = Category.find(params[:id])
+        category.update_attributes!(category_params)
+
+        respond_with category
+      end
+
       def show
         respond_with Category.find(params[:id])
       end
+
+      private
+        def category_params
+          params.require(:category).permit(:id, :name, :description, :slug)
+        end
     end
   end
 end
--- spec/controllers/landable/api/categories_controller_spec.rb
+++ spec/controllers/landable/api/categories_controller_spec.rb
@@ -41,5 +41,55 @@ module Landable::Api
         last_json['categories'].size.should == 5
       end
     end
+
+    describe '#create' do
+      include_examples 'Authenticated API controller', :make_request
+
+      let(:default_params) do
+        { category: attributes_for(:category) }
+      end
+
+      let(:category) do
+        Landable::Category.where(name: default_params[:category][:name]).first
+      end
+
+      def make_request(params = {})
+        post :create, default_params.deep_merge(category: params)
+      end
+
+      context 'success' do
+        it 'returns 201 Created' do
+          make_request
+          response.status.should == 201
+        end
+
+        it 'returns header Location with the category URL' do
+          make_request
+          response.headers['Location'].should == category_url(category)
+        end
+
+        it 'renders the category as JSON' do
+          make_request
+          name, description = default_params[:category].values_at(:name, :description)
+          last_json['category'].should include('name' => name, 'description' => description)
+        end
+      end
+    end
+
+    describe '#update' do
+      include_examples 'Authenticated API controller', :make_request
+
+      let(:category) { create :category }
+
+      def make_request
+        put :update, id: category.id, category: { description: "Updated" }
+      end
+
+      it "updates the asset description" do
+        make_request
+        category.reload
+        category.description.should == "Updated"
+      end
+    end
   end
 end
