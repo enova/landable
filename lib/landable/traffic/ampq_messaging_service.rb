c72b38c017e4d8387864dee8bd0c07d7d498b82b
Calvin Mwesigwa <cmwesigw@gmail.com>
2015-04-21 18:22:23 -0500

    ampq_with_bunny: works now with simplic

--- lib/landable/traffic/ampq_messaging_service.rb
+++ lib/landable/traffic/ampq_messaging_service.rb
@@ -3,7 +3,14 @@ module Landable
     def initialize(ampq_configuration)
       @bunny = Bunny.new(ampq_configuration)
       @bunny.start
+      @bunny_ch = @bunny.create_channel
+      # @bunny_queue = @bunny_ch.queue(ampq_queue)
       at_exit { @bunny.stop }
+    end
+
+    def publish(message)
+      @bunny_queue = @bunny_ch.queue(Landable.configuration.ampq_queue)
+      @bunny_queue.publish(message.to_s)
     end
   end
 end
