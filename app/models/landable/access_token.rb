4585eaccb97360353cd40470df84449bab608270
Sal Ortiz <sortiz@enova.com>
2015-06-26 15:16:18 +0000

    moved hardcoded session timout

--- app/models/landable/access_token.rb
+++ app/models/landable/access_token.rb
@@ -2,20 +2,23 @@ module Landable
   class AccessToken < ActiveRecord::Base
     include Landable::TableName
 
+    # Maximum Token age, in hours.
+    MAX_AGE = Landable::configuration['ldap'][:access_token_max_age]
+
     belongs_to :author
     validates_presence_of :author_id
     validates_presence_of :expires_at
     validates_presence_of :permissions
 
     before_validation do |token|
-      token.expires_at ||= 8.hours.from_now
+      token.expires_at ||= MAX_AGE.hours.from_now
     end
 
     scope :fresh,   -> { where('expires_at > ?',  Time.zone.now) }
     scope :expired, -> { where('expires_at <= ?', Time.zone.now) }
 
     def refresh!
-      update_column :expires_at, 8.hours.from_now
+      update_column :expires_at, MAX_AGE.hours.from_now
     end
 
     def can_publish?
