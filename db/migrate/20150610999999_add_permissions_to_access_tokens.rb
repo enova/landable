a9bdf1dfeb94d89fb6a7144b16041452e3052741
Sal Ortiz <sortiz@enova.com>
2015-07-07 14:38:46 +0000

    removed unnecessary migration option

--- db/migrate/20150610999999_add_permissions_to_access_tokens.rb
+++ db/migrate/20150610999999_add_permissions_to_access_tokens.rb
@@ -1,6 +1,6 @@
 class AddPermissionsToAccessTokens < ActiveRecord::Migration
   def change
     enable_extension 'hstore' unless extension_enabled?('hstore')
-    add_column "#{Landable.configuration.database_schema_prefix}landable.access_tokens", :permissions, :hstore, array: true
+    add_column "#{Landable.configuration.database_schema_prefix}landable.access_tokens", :permissions, :hstore
   end
 end
