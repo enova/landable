c33821b5a73b6b6e7550a5e6c905c1d4df21a966
Sal Ortiz <sortiz@enova.com>
2015-07-29 21:56:29 +0000

    updated migration to include template_revision

--- db/migrate/20150728195345_add_category_column_to_templates.rb
+++ db/migrate/20150728195345_add_category_column_to_templates.rb
@@ -1,7 +1,13 @@
 class AddCategoryColumnToTemplates < ActiveRecord::Migration
   def change
     schema_name = "#{Landable.configuration.database_schema_prefix}landable"
+
+    # add category_id to our templates table.
     add_column "#{schema_name}.templates", :category_id, :uuid
     execute "ALTER TABLE #{schema_name}.templates ADD FOREIGN KEY (category_id) REFERENCES #{schema_name}.categories(category_id)"
+
+    # add category_id to our template_revisions table.
+    add_column "#{schema_name}.template_revisions", :category_id, :uuid
+    execute "ALTER TABLE #{schema_name}.template_revisions ADD FOREIGN KEY (category_id) REFERENCES #{schema_name}.categories(category_id)"
   end
 end
