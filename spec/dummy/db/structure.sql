f09519a4a460c18c741be809354e0d232c73e660
Paul Haddad <paulh16@gmail.com>
2015-07-31 11:23:29 -0500

    Update Changelog and add sql file for specs

--- spec/dummy/db/structure.sql
+++ spec/dummy/db/structure.sql
@@ -162,7 +162,7 @@ CREATE TABLE access_tokens (
     expires_at timestamp without time zone NOT NULL,
     created_at timestamp without time zone,
     updated_at timestamp without time zone,
-    permissions character varying[]
+    permissions public.hstore
 );
 
 
