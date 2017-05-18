c95e1590110526cf089beb86bd6a71244661d662
Matt Larraz <mlarraz@enova.com>
2017-05-18 16:06:31 -0500

    Require rack-schema directly

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,6 +8,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require 'simplecov'
 SimpleCov.start 'rails'
 
+require 'rack-schema'
+
 require File.expand_path('../../../spec/dummy/config/environment.rb', __FILE__)
 ENV['RAILS_ROOT'] ||= File.dirname(__FILE__) + '../../../spec/dummy'
 
